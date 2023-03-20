35b44f6007048cb40c27f8691f75e464e570df97
marcelsev <marcelosolanosev@gmail.com>
2023-03-20 17:15:36 +0100

    first commit

---
+++ funcion.js
@@ -0,0 +1,8 @@
+function init (){
+    let resultado = document.getElementById("resultado");
+    let suma = document.getElementById("suma");
+    let resta = document.getElementById("resta");
+    let division = document.getElementById("division");
+    let multiplicacion = document.getElementById("multiplicacion");
+    
+}
